c80bfe78c90415bd220c8d7f55708ff1d8ee7726
Adrien Cacciaguerra <adrien.caccia@gmail.com>
2020-04-25 16:58:31 +0200

    ✨ Add hello route logic

--- app.py
+++ app.py
@@ -1,6 +1,26 @@
 from flask import Flask
+from datetime import datetime
+import re
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route("/")
+def home():
+    return "Hello, Flask!"
+
+@app.route("/hello/<name>")
+def hello_there(name):
+    now = datetime.now()
+    formatted_now = now.strftime("%A, %d %B, %Y at %X")
+
+    # Filter the name argument to letters only using regular expressions. URL arguments
+    # can contain arbitrary text, so we restrict to safe characters only.
+    match_object = re.match("[a-zA-Z]+", name)
+
+    if match_object:
+        clean_name = match_object.group(0)
+    else:
+        clean_name = "Friend"
+
+    content = "Hello there, " + clean_name + "! It's " + formatted_now
+    return content
